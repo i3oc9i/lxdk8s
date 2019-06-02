cff592468d3fbf3efbcb6b46f64e7285367f5102
i3oc9i <ivano.coltellacci@gmail.com>
2019-06-02 14:45:44 +0200

    Rename task files

--- lxdk8s-bootstrap.sh
+++ lxdk8s-bootstrap.sh
@@ -6,13 +6,13 @@ set -eu
 
 ./00-bootstrap-lxd.sh
 ./01-get-client-tools.sh
-./10-gen-certs.sh
-./11-gen-configs.sh
-./12-gen-encryption.sh
+./10-generate-certs.sh
+./11-generate-kubeconfigs.sh
+./12-generate-encryption-keys.sh
 ./20-bootstrap-haproxy.sh
 ./30-bootstrap-etcd.sh
 ./31-bootstrap-control-plane.sh
-./32-setup-rbac-kubelet.sh
+./32-configure-rbac-kubelet-authorization.sh
 ./40-bootstrap-workers.sh
 ./50-create-network-routes.sh
 
