22f7a112f308684de209cb6d0648ed8dc10d6073
i3oc9i <ivano.coltellacci@gmail.com>
2019-06-02 14:00:04 +0200

    remove shell set -x option

--- 40-bootstrap-workers.sh
+++ 40-bootstrap-workers.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2019 Ivano Coltellacci. All rights reserved.
 # Use of this source code is governed by the MIT License that can be found in
 # the LICENSE file.
-set -eux
+set -eu
 
 source ./config/lxdk8s.config
 
